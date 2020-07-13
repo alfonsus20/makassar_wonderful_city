48c9b3aec6c78b9db1cc9779561cf118d6974edc
alfonsus20 <58034788+alfonsus20@users.noreply.github.com>
2020-07-13 21:57:42 +0800

    Add files via upload

---
+++ asset/JS/comment.js
@@ -0,0 +1,66 @@
+// var data = {
+//   nama: "",
+//   komentar: "",
+//   id: null,
+// };
+
+function hapus() {
+  document.getElementById("input-nama").value = "";
+  document.getElementById("komentar").value = "";
+}
+
+function kirim() {
+  if (
+    document.getElementById("input-nama").value &&
+    document.getElementById("komentar").value
+  ) {
+    const data = {
+      nama: document.getElementById("input-nama").value,
+      komentar: document.getElementById("komentar").value,
+    };
+    console.log(data);
+    setComments(data);
+    renderComments();
+    hapus();
+  } else {
+    alert("Tolong Cek Input Anda");
+  }
+}
+
+const cache_key = "all_comments";
+
+function setComments(comment_data) {
+  let comments = null;
+  if (localStorage.getItem(cache_key) === null) {
+    comments = [];
+  } else {
+    comments = JSON.parse(localStorage.getItem(cache_key));
+  }
+  comments.unshift(comment_data);
+  localStorage.setItem(cache_key, JSON.stringify(comments));
+}
+
+function showComments() {
+  if (typeof Storage !== "undefined") {
+    return JSON.parse(localStorage.getItem(cache_key)) || [];
+  } else {
+    return [];
+  }
+}
+
+function renderComments() {
+  const comments = showComments();
+  let commentList = document.querySelector("#comments-container");
+  commentList.innerHTML = "";
+  for (let comment of comments) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "comment-content");
+    row.innerHTML = "<img src=./asset/gambar/profile.png>";
+    let row2 = document.createElement("div");
+    row2.setAttribute("class", "comments-inner");
+    row2.innerHTML += "<div class=name>" + comment.nama + "</div>";
+    row2.innerHTML += "<div class=comment>" + comment.komentar + "</div>";
+    row.appendChild(row2);
+    commentList.appendChild(row);
+  }
+}
